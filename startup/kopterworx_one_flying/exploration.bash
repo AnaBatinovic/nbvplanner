aa9334f9247bdde25c46fa5cdb7b8e665f16430b
Ana Batinovic <ana.batinovic.ab@gmail.com>
2021-09-02 07:38:58 +0200

    Edit params for maze world, edit auto mode

--- startup/kopterworx_one_flying/exploration.bash
+++ startup/kopterworx_one_flying/exploration.bash
@@ -5,5 +5,9 @@ start_path=/home/ana/nbvp_ws/src/nbvplanner/startup/kopterworx_one_flying
 
 for max_range in {0..10..1}
   do
-    ./start.sh    
+    rosbag record -o original-maze /red/octomap_volume /red/comp_times __name:=my_bag &
+    ./start.sh 
+    rosnode kill /my_bag
+    killall gzserver
+    done
 done
